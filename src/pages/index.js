37ed813961423e45768631aac99ea2c2d2523e1c
drumfeet <drumfeet@gmail.com>
2024-08-01 13:20:50 +0200

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,42 @@ import {
   result,
 } from "@permaweb/aoconnect"
 import { Button, Flex, Heading, Text, useToast } from "@chakra-ui/react"
+import { getStakers } from "@/lib/utils"
 
-const FLIP_PID = "wVmuJvMKfK7bTfo3hxfIvvOjQfufKSueYBspt2t74Ck"
+const FLIP_PID = process.env.NEXT_PUBLIC_PROCESS_ID
+const limit = 10
+export default function Home() {
+  const [skip, setSkip] = useState(0)
 
-export default function main(){
+  const loadStakers = async () => {
+    try {
+      await getStakers({ limit, skip })
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
-  return(
+  const loadMatches = async () => {
+    try {
+      await getMatches({ limit, skip })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  return (
     <>
-    Home
+      <Flex minH="100%" direction="column" p={4}>
+        <Flex alignItems="center" flexDirection="column" gap={4}>
+          <Text>{FLIP_PID}</Text>
+          <Button variant="outline" onClick={loadStakers}>
+            Get Stakers
+          </Button>
+          <Button variant="outline" onClick={loadMatches}>
+            Get Matches
+          </Button>
+        </Flex>
+      </Flex>
     </>
   )
 }
