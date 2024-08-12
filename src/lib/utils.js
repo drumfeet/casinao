b2dcbb017c977f72f3118b9cb57ae64ad71f41f3
drumfeet <drumfeet@gmail.com>
2024-08-12 19:54:39 +0200

    wip

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -34,7 +34,7 @@ const getGameBalance = async ({ recipient }) => {
   ]
 
   const result = await dryrun({
-    process: "Wu7s2PCoBt1-38dgtCwiGfCtK5V1DtxHpgK1KcYxQiQ", // GAME_PROCESS_ID
+    process: process.env.NEXT_PUBLIC_GAME_PID,
     tags,
   })
   console.log("getGameBalance result", result)
@@ -49,7 +49,7 @@ const getWalletBalance = async ({ recipient }) => {
   ]
 
   const result = await dryrun({
-    process: "_JZTfLS-ssyKKNn-qMb7PSifdo_1SZ14UlI_RRg-nfo", // WAR_PROCESS_ID
+    process: process.env.NEXT_PUBLIC_TOKEN_PID,
     tags,
   })
   console.log("getWalletBalance result", result)
